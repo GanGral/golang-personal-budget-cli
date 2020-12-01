4f82df7af72a0a4ea0f331ca94c812b4492e1024
Igor Korablev <Ikorablev@osisoft.com>
2020-12-01 14:51:05 -0500

    Step 1

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+var Bugdet struct {
+	Max   float32
+	Items []Item
+}
 
 // Item stores item information
+var Item struct {
+	Description string
+	Price       float32
+}
